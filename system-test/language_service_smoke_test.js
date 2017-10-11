0197c73d926fd77460da821bf4bd7f03af7d6b08
Luke Sneeringer <lukesneeringer@google.com>
2017-10-11 07:36:34 -0700

    Updates to auto-gen surface.
    Based in googleapis/toolkit#1604, googleapis/toolkit#1607, and googleapis/toolkit#1609.

--- system-test/language_service_smoke_test.js
+++ system-test/language_service_smoke_test.js
@@ -1,40 +1,40 @@
-/*
- * Copyright 2017, Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2017, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
-describe('LanguageServiceSmokeTest', function() {
+describe('LanguageServiceSmokeTest', () => {
 
-  it('successfully makes a call to the service', function(done) {
-    var language = require('../src');
+  it('successfully makes a call to the service', done => {
+    const language = require('../src');
 
-    var client = language.v1({
+    var client = new language.v1.LanguageServiceClient({
       // optional auth parameters.
     });
 
     var content = 'Hello, world!';
-    var type = language.v1.types.Document.Type.PLAIN_TEXT;
+    var type = 'PLAIN_TEXT';
     var document = {
-        content : content,
-        type : type
+      content: content,
+      type: type,
     };
-    client.analyzeSentiment({document: document}).then(function(responses) {
+    client.analyzeSentiment({document: document})
+      .then(responses => {
         var response = responses[0];
         console.log(response);
-    })
-    .then(done)
-    .catch(done);
+      })
+      .then(done)
+      .catch(done);
   });
 });
