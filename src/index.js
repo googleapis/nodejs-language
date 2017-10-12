aaea90edd007df05fc37acbbc2d003c59e25be12
Luke Sneeringer <lukesneeringer@google.com>
2017-10-12 09:16:55 -0700

    Add new autogen based on toolkit 1609, 1614

--- src/index.js
+++ src/index.js
@@ -1,28 +1,72 @@
+// Copyright 2017, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
- * Copyright 2017, Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * @namespace google
+ */
+/**
+ * @namespace google.cloud
+ */
+/**
+ * @namespace google.cloud.language
+ */
+/**
+ * @namesapce google.cloud.language.v1
  */
 
 'use strict';
 
 // Import the clients for each version supported by this package.
-var gapic = {
+let gapic = {
   v1: require('./v1'),
-  v1beta2: require('./v1beta2'),
 };
 
+/**
+ * The `@google-cloud/language` package has the following named exports:
+ *
+ * `LanguageServiceClient` - Reference to {@link v1.@client.clientName}}
+ * - `v1` - This is used for selecting or pinning a
+ *   particular backend service version. It exports:
+ *     - `` - Reference to {@link v1.@client.clientName}}
+ *
+ * @module {object} @google-cloud/language
+ * @alias nodejs-language
+ *
+ * @example <caption>Install the client library with
+ *   <a href="https://www.npmjs.com/">npm</a>:</caption>
+ * npm install --save @google-cloud/language
+ *
+ * @example <caption>Import the client library:</caption>
+ * const language = require('@google-cloud/language');
+ *
+ * @example <caption>Create a client:</caption>
+ * let client = new language.LanguageServiceClient();
+ */
+
+/**
+ * @type {object}
+ * @property {constructor} LanguageServiceClient
+ *   Reference to {@link v1.LanguageServiceClient}
+ */
 module.exports = gapic.v1;
+
+/**
+ * @type {object}
+ * @property {constructor} LanguageServiceClient
+ *   Reference to {@link v1.LanguageServiceClient}
+ */
 module.exports.v1 = gapic.v1;
-module.exports.v1beta2 = gapic.v1beta2;
+
+// Alias `module.exports` as `module.exports.default`, for future-proofing.
 module.exports.default = Object.assign({}, module.exports);
