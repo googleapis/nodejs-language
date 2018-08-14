46b4734e8bd44d0247a81953e9212a59279a63c8
Jonathan Lui <jonathanlui@google.com>
2018-08-14 13:44:08 -0700

    build: add repo_name to synth.py

--- synth.py
+++ synth.py
@@ -10,17 +10,15 @@ common_templates = gcp.CommonTemplates()
 
 # tasks has two product names, and a poorly named artman yaml
 for version in ['v1', 'v1beta2']:
-    library = gapic.node_library(
-        'language', version)
+    library = gapic.node_library('language', version)
 
     # skip index, protos, package.json, and README.md
-    s.copy(
-        library,
-        excludes=['package.json', 'README.md', 'src/index.js'])
+    s.copy(library, excludes=['package.json', 'README.md', 'src/index.js'])
 
-templates = common_templates.node_library(package_name="@google-cloud/language")
+templates = common_templates.node_library(
+    package_name="@google-cloud/language",
+    repo_name="googleapis/nodejs-language")
 s.copy(templates)
-
 
 # Node.js specific cleanup
 subprocess.run(['npm', 'install'])
